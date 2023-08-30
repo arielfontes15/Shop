5cb0d3dc36ffeb1c942ca5dc859693719ae6137f
Ariel Vitor <arielfontes15@hotmail.com>
2023-08-29 23:23:28 -0300

    Criando nosso modelo USER

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.User {
+    public class User {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(20, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(20, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
