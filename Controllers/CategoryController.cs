f6d79911724a99fc0997d6a6f56abd2c78aa9fd2
Ariel Vitor Fontes Leal <arielfontes15@hotmail.com>
2023-10-18 23:29:45 -0300

    Criando meu DbContext e iniciando minhas migrations

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
 using Shop.Models;
 
 [Route("categories")]
@@ -6,37 +8,97 @@ public class CategoryController :  ControllerBase {
 
     [HttpGet]
     [Route("")]
-    public async Task<ActionResult<List<Category>>> GetCategory() {
-        return new List<Category>();
+    public async Task<ActionResult<List<Category>>> GetCategory(
+        [FromServices] DataContext context
+    ) {
+        var categories = await context.Categories.AsNoTracking().ToListAsync();
+        return Ok(categories);
     }
     
     [HttpGet]
     [Route("{id:int}")]
-    public async Task<ActionResult<Category>> GetByIdCategory(string id) {
-        return new Category();
+    public async Task<ActionResult<Category>> GetByIdCategory(
+        string id,
+        [FromServices] DataContext context
+    ) {
+        try
+        {
+            var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync(c => c.Id == int.Parse(id));
+            return Ok(category);
+        }
+        catch (System.Exception)
+        {
+            return BadRequest(new { message = "Não foi possivel encontrar esta categoria."});
+        }
     }
 
     [HttpPost]
     [Route("")]
-    public async Task<ActionResult<List<Category>>> PostCategory([FromBody]Category model) {
+    public async Task<ActionResult<List<Category>>> PostCategory(
+        [FromBody]Category model,
+        [FromServices] DataContext context
+    ) {
         if(!ModelState.IsValid)
             return BadRequest(ModelState);
-        return Ok(model);
+
+        try
+        {
+            context.Categories.Add(model);
+            await context.SaveChangesAsync();
+            return Ok(model);
+        }
+        catch
+        {
+            return BadRequest(new { message = "Não foi possivel criar a categoria."});
+        }
+
     }
 
     [HttpPut]
     [Route("{id:int}")]
-    public async Task<ActionResult<List<Category>>> PutCategory(int id, [FromBody]Category model) {
+    public async Task<ActionResult<List<Category>>> PutCategory(
+        int id,
+        [FromBody]Category model,
+        [FromServices] DataContext context
+    ) {
         if(model.Id != id)
-            return NotFound(new { message = "Categoria não encontrada" });
+            return NotFound(new { message = "Categoria não encontrada." });
         if(!ModelState.IsValid)
             return BadRequest(ModelState);
-        return Ok(model);
+        try
+        {
+            context.Entry<Category>(model).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(model);
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            return BadRequest(new { message = "Este registro ja foi atualizado."});
+        }
+        catch (Exception)
+        {
+            return BadRequest(new { message = "Não foi possivel atualizar a categoria."});
+        }
     }
 
     [HttpDelete]
     [Route("{id:int}")]
-    public  async Task<ActionResult<List<Category>>> DeleteCategory(int id) {
-        return Ok();
+    public  async Task<ActionResult<List<Category>>> DeleteCategory(
+        int id,
+        [FromServices] DataContext context
+    ) {
+        var category = await context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+        if(category == null)
+            return BadRequest(new { message = "Não foi possivel remover a categoria."});
+        try
+        {
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
+            return Ok(new { message = "Categoria removida com sucesso." });
+        }
+        catch (System.Exception)
+        {
+            return BadRequest(new { message = "Não foi possivel remover a categoria."});
+        }
     }
 }
---
+++ Migrations/20231018185100_InitialCreate.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Shop.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Password = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    CategoryId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
